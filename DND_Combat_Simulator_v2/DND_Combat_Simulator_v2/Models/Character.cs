866143c5a342cf47945dd43c362f83b544660a35
Matt Eland <matt.eland@gmail.com>
2022-09-05 22:08:29 -0400

    Added Required Attribute

--- DND_Combat_Simulator_v2/DND_Combat_Simulator_v2/Models/Character.cs
+++ DND_Combat_Simulator_v2/DND_Combat_Simulator_v2/Models/Character.cs
@@ -1,9 +1,12 @@
-﻿namespace DND_Combat_Simulator_v2.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DND_Combat_Simulator_v2.Models
 {
     public class Character
     {
         public int Id { get; set; }
         public string? Name { get; set; }
+        [Required]
         public Race? Race { get; set; }
         public int Strength { get; set; }
         public int Dexterity { get; set; }
@@ -11,6 +14,7 @@
         public int Intelligence { get; set; }
         public int Wisdom { get; set; }
         public int Charisma { get; set; }
+        [Required]
         public Weapon? Weapon { get; set; }
     }
 }
